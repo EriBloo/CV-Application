db5d33e81bcd4c514129d68aaf6048299270d54b
eriBloo <19932449+EriBloo@users.noreply.github.com>
2020-10-15 09:55:18 +0200

    Updates

---
+++ precache-manifest.4c2b0fe9642faec22f3d93340f47bbca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a44500a9dabb3d62d0014e8af768a99",
+    "url": "/CV-application/index.html"
+  },
+  {
+    "revision": "7f8e143e6023005b88a7",
+    "url": "/CV-application/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "23d12cdf57a88572d51b",
+    "url": "/CV-application/static/css/main.ec15bb35.chunk.css"
+  },
+  {
+    "revision": "7f8e143e6023005b88a7",
+    "url": "/CV-application/static/js/2.02d41be6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CV-application/static/js/2.02d41be6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d12cdf57a88572d51b",
+    "url": "/CV-application/static/js/main.2525b42c.chunk.js"
+  },
+  {
+    "revision": "268d253a9f8f0b1fb39e",
+    "url": "/CV-application/static/js/runtime-main.9ad69c9d.js"
+  }
+]);
